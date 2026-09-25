bcc9760e15073f1ee0e661c54212bb68977f3cf9
Roger S. Mikalsen <rmikalsen@hotmail.com>
2026-09-25 11:23:12 +0200

    siste

--- Kapittel1/1E Ordbøker/oppgaver/oppgave4.py
+++ Kapittel1/1E Ordbøker/oppgaver/oppgave4.py
@@ -1,3 +1,4 @@
+import random 
 dna_rna = {"A":"U",
            "T":"A",
            "C":"G",
@@ -39,29 +40,46 @@ kodontabell = {
 }
 
 dna_sekvens = "GCCCTCCAGGACAGGCTGCATCAGAAGAGGCCATCAAGCAGGTCTGTTCCAAGGGCCTTTGCGTCAGGTGGGCTCAGGATTCCAGGGTGGCTGGACAGC"
-#Endret en bokstav
-mutert_endret_dna_sekvens = "GCCCTCCAGGACAGGCTGCATCAGAAGAGGCCATCAAGCAGGTCTGTTCCAAGGGCCTTTGCGTCAGGTGGGCTCAGGATTCCAGGGTGGCTGGACAGC"
-#Fjernet en bokstav
-mutert_fjernet_dna_sekvens = "GCCCTCCAGGACAGGTGCATCAGAAGAGGCCATCAAGCAGGTCTGTTCCAAGGGCCTTTGCGTCAGGTGGGCTCAGGATTCCAGGGTGGCTGGACAGCA"
+#Mutasjon 1: Endret en bokstav
+nybokstav = random.choice(["A", "C", "G", "T"])
+endrer_index = random.randint(0,int(len(dna_sekvens)/2)) #Endrer en i 1. halvdel
+while nybokstav == dna_sekvens[endrer_index]:
+    nybokstav = random.choice(["A", "C", "G", "T"])
+print(f"Muterer ved å endre: {endrer_index+1} til {nybokstav}")
+mutasjon1_dna_sekvens = dna_sekvens[0:endrer_index]+nybokstav + dna_sekvens[endrer_index+1::]
+#Mutasjon 2: Fjernet en bokstav
+print(f"Muterer ved å fjerne: {endrer_index+1} og legger til på slutten {nybokstav}")
+mutasjon2_dna_sekvens = dna_sekvens[0:endrer_index]+dna_sekvens[endrer_index+1::]+nybokstav
 
 rna_sekvens = "".join([dna_rna[syre] for syre in dna_sekvens])
-mutert_endret_rna_sekvens = "".join([dna_rna[syre] for syre in mutert_endret_dna_sekvens])
-mutert_fjernet_rna_sekvens = "".join([dna_rna[syre] for syre in mutert_fjernet_dna_sekvens])
+mutasjon1_rna_sekvens = "".join([dna_rna[syre] for syre in mutasjon1_dna_sekvens])
+mutasjon2_rna_sekvens = "".join([dna_rna[syre] for syre in mutasjon2_dna_sekvens])
 
 
-print(f"a)  DNA-sekvens: {dna_sekvens}\ngir RNA-sekvens: {rna_sekvens}")
+print(f"a)  DNA-sekvens:  {dna_sekvens}\n"
+      f"gir RNA-sekvens:  {rna_sekvens}\n"
+      f"Mutert DNA1 sekv: {mutasjon1_dna_sekvens}"
+      f"Mutert RNA1 sekv: {mutasjon1_rna_sekvens}\n"
+      f"Mutert DNA2 sekv: {mutasjon2_dna_sekvens}"
+      f"Mutert RNA2 sekv: {mutasjon2_rna_sekvens}")
 
 antall_aminosyrer = 0
 print()
-print(f"{'Orginal RNA':15}|{'Mutasjon 1':15}|{'Mutasjon 2':15}")
+print(f'{"-"*15}+{"-"*15}+{"-"*15}+{"-"*15}')
+print(f"{'Triplet':15}|{'Orginal RNA':15}|{'Mutasjon 1':15}|{'Mutasjon 2':15}")
+print(f'{"-"*15}+{"-"*15}+{"-"*15}+{"-"*15}')
+
 for i in range(0, len(rna_sekvens),3):
     aminosyre = kodontabell[rna_sekvens[i:i+3]]
 #    if aminosyre == "Stop":
 #        break
     antall_aminosyrer +=1
-    print(f"{aminosyre:15}|"
-          f"{kodontabell[mutert_endret_rna_sekvens[i:i+3]]:15}|"
-          f"{kodontabell[mutert_fjernet_rna_sekvens[i:i+3]]:15}")
+    print(f"{'('+ str(i+1) + '-' + str(i+3) + ')':15}|"
+          f"{aminosyre:15}|"
+          f"{kodontabell[mutasjon1_rna_sekvens[i:i+3]]:15}|"
+          f"{kodontabell[mutasjon2_rna_sekvens[i:i+3]]:15}")
+
+print(f'{"-"*15}+{"-"*15}+{"-"*15}+{"-"*15}')
 
 print(antall_aminosyrer)
 
